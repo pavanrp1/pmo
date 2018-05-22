cda6a762db77adfc2259d8af8e26f0631db4ed45
Pavan Kumar RP <pavan.kumarrp@cerner.com>
2018-05-22 12:18:23 +0530

    Initial Check-in

---
+++ src/main/java/com/cts/infraoss/pmo/model/User.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.cts.infraoss.pmo.model;
+
+/**
+ * @author pk015603
+ *
+ */
+public class User {
+	private String userName;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+}
